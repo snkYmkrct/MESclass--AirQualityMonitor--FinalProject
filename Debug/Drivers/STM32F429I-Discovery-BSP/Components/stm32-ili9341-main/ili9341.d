6e94ceb7646bb0cc72167655ecab3bbef5d1fdcd
Vi <silvia.copil@gmail.com>
2022-05-23 18:16:16 +0200

    initial commit

--- Debug/Drivers/STM32F429I-Discovery-BSP/Components/stm32-ili9341-main/ili9341.d
+++
@@ -1,6 +0,0 @@
-Drivers/STM32F429I-Discovery-BSP/Components/stm32-ili9341-main/ili9341.o: \
- ../Drivers/STM32F429I-Discovery-BSP/Components/stm32-ili9341-main/ili9341.c \
- ../Drivers/STM32F429I-Discovery-BSP/Components/stm32-ili9341-main/ili9341.h \
- ../Drivers/STM32F429I-Discovery-BSP/Components/stm32-ili9341-main/../Common/lcd.h
-../Drivers/STM32F429I-Discovery-BSP/Components/stm32-ili9341-main/ili9341.h:
-../Drivers/STM32F429I-Discovery-BSP/Components/stm32-ili9341-main/../Common/lcd.h:
--- Debug/Drivers/STM32F429I-Discovery-BSP/Components/stm32-stmpe811-main/stmpe811.d
+++
@@ -1,8 +0,0 @@
-Drivers/STM32F429I-Discovery-BSP/Components/stm32-stmpe811-main/stmpe811.o: \
- ../Drivers/STM32F429I-Discovery-BSP/Components/stm32-stmpe811-main/stmpe811.c \
- ../Drivers/STM32F429I-Discovery-BSP/Components/stm32-stmpe811-main/stmpe811.h \
- ../Drivers/STM32F429I-Discovery-BSP/Components/stm32-stmpe811-main/../Common/ts.h \
- ../Drivers/STM32F429I-Discovery-BSP/Components/stm32-stmpe811-main/../Common/io.h
-../Drivers/STM32F429I-Discovery-BSP/Components/stm32-stmpe811-main/stmpe811.h:
-../Drivers/STM32F429I-Discovery-BSP/Components/stm32-stmpe811-main/../Common/ts.h:
-../Drivers/STM32F429I-Discovery-BSP/Components/stm32-stmpe811-main/../Common/io.h:
